4667af832d451c2c73339756a9b1630cca3dd5f3
Muhammad Adnan <its.mohammadadnan@gmail.com>
2025-12-01 18:14:42 +0000

    Git push test 2

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,7 +2,7 @@ from app.database import Base, engine
 from .user import User
 from .chat import ChatSession, Message
 
-Base.metadata.drop_all(bind = engine)
-Base.metadata.create_all(bind=engine)
+# Base.metadata.drop_all(bind = engine)
+# Base.metadata.create_all(bind=engine)
 
 __all__ = ["User","ChatSession","Message"]
